c35b89db5cc7a1ffc49b242b300e49a4ec211bea
Abdael17 <abdael172006@gmail.com>
2026-02-19 01:55:43 -0500

    Update script.js

--- script.js
+++ script.js
@@ -496,7 +496,7 @@ function drawGameOverScreen() {
     ctx.fillStyle = "#ff4d6d";
     ctx.textAlign = "center";
     ctx.font = "60px 'VT323'";
-    ctx.fillText("!NO TE RINDAS 😿¡ AYUDAME A LLEGAR A MI MONITA ", canvas.width/2, canvas.height/2 - 20);
+    ctx.fillText("!TU PUEDES MONA 🐵¡", canvas.width/2, canvas.height/2 - 20);
     
     ctx.fillStyle = "white";
     ctx.font = "30px 'VT323'";
@@ -662,6 +662,7 @@ if(prevBtn) prevBtn.addEventListener('click', prevSong);
 
 // Cargar primera canción
 loadSong(playlist[currentSongIndex]);
+
 
 
 
